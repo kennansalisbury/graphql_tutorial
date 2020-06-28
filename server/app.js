ee396bfaae85f1df0628fcf99579c7cccd5b4480
kennansalisbury <kennansalisbury@gmail.com>
2020-06-28 14:31:31 -0400

    books and authors included in schema

--- server/app.js
+++ server/app.js
@@ -3,13 +3,16 @@ const express = require('express')
 //express-graphql allows express server to communicate with and undesrtand graphql
     //use this as a middleware on our query, this acts a single endpoint for querying the data
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express();
 
 //graphql middleware
 app.use('./graphql', graphqlHTTP({
     //pass schema that tells express graphql about our data and how our graph will look
-    
+    //same as schema: schema
+    schema,
+    graphiql: true
 }))
 
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,79 @@
 //define schema of data
 
 const graphql = require('graphql')
+const _ = require('lodash')
 
 //destructuring takes the variable from graphql
-const {GraphQLObjectType} = graphql
+const {
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt
+} = graphql
 
-const BookType = new GraphQLObjectType
+//dummy data
+let books = [
+    {name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'}
+]
+
+let authors = [
+    {name: 'Patrick Rothfuss', age: 44, id: '1'},
+    {name: 'Brandon Sanderson', age: 42, id: '2'},
+    {name: 'Terry Pratchett', age: 66, id: '3'}
+]
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        //this is a function because later when we have multiple types referencing each other
+            //unless fields are wrapped in a function one type might not know what another type is
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        genre: { type: GraphQLString}
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        //this is a function because later when we have multiple types referencing each other
+            //unless fields are wrapped in a function one type might not know what another type is
+        id: {type: GraphQLID},
+        name: { type: GraphQLString},
+        age: {type: GraphQLInt}
+    })
+})
+
+//root queries - where we access the data graph (ex: books, authors)
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        //name of how it will be queried from the frontend
+        book: {
+            type: BookType,
+            //these are the arguments I expect them to pass along to identify which book is being queried
+            args: { id: {type: GraphQLID}},
+            resolve(parent, args) {
+                //code to get data from db or other source
+                //parent will come into play when we have relationships
+                //args.id used to access the id into our database query code
+                return _.find(books, {id: args.id})
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return _.find(authors, {id: args.id})
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    //initial root query for this schema
+    query: RootQuery
+})
