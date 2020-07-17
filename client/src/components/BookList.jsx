27f6f0af073ed4c61149cbc652abacdb9cdd4d41
kennansalisbury <kennansalisbury@gmail.com>
2020-07-17 16:45:42 -0700

    initial react app created

---
+++ client/src/components/BookList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BookList = props => {
+    return (
+        <div>
+            <ul id="book-list">
+                <li>Book name</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList
